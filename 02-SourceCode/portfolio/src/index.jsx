23e9c3d071caf8b6f4d661d59c809cbde9b5b690
Damien Loup <damien.loup@abraxas.ch>
2023-05-30 11:53:49 +0200

    Modified router

--- 02-SourceCode/portfolio/src/index.jsx
+++ 02-SourceCode/portfolio/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from'react-router-dom';
+import { HashRouter as Router, Routes, Route } from'react-router-dom';
 
 import Header from './pages/includes/Header';
 import Footer from './pages/includes/Footer';
