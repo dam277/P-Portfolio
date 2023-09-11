67b2cb9524e199defa3082f25ab29931b77242fd
dam277 <damis.loup@gmail.com>
2023-09-11 06:27:54 +0200

    Modified the lang context

--- 02-SourceCode/portfolio/src/utils/contexts/LangContext.jsx
+++ 02-SourceCode/portfolio/src/utils/contexts/LangContext.jsx
@@ -1,11 +1,20 @@
+// Import Hooks
 import { useState, createContext } from "react"
 
+// Create Context
 export const LangContext = createContext()
 
+/**
+ * Lang provider
+ * @param {HTMLElement} children => Children element to display
+ * @returns {HTMLElement} component html elements
+ */
 const LangProvider = ({ children }) => 
 {
-    const [language, setLanguage] = useState("EN");
+    // Set States
+    const [language, setLanguage] = useState("EN");         // Current language of the website
 
+    // Return html elements
     return (
         <LangContext.Provider value={{ language, setLanguage }}>
             {children}
